a9aef734446519d2bac59f381e519761809c2673
lingjuan.guan <lingjuan.guan@qunar.com>
2015-12-15 23:55:20 +0800

    add

--- gulpfile.js
+++ gulpfile.js
@@ -6,17 +6,33 @@ var buffer = require('vinyl-buffer');
 
 gulp.task("browserify", function () {
     var b = browserify({
-        entries: "./src/scripts/page/login.js",
+        entries: "src/scripts/page/login.js",
         debug: true
     });
-
+    console.log(b);
     return b.bundle()
-        .pipe(source("logins.js"))
+        .pipe(source("./login.js"))
         .pipe(buffer())
         .pipe(sourcemaps.init({loadMaps: true}))
         .pipe(sourcemaps.write("."))
-        .pipe(gulp.dest("../../prd"));
+        .pipe(gulp.dest("./src/prd"));
 });
+
+
+	// var b = browserify({
+	//     entries: './script/app.js',
+	//     debug: true
+	// });
+    //
+	// return b.bundle()
+    // .pipe(source('./bundle.js'))
+    // .pipe(buffer())
+    // .pipe(sourcemaps.write('./'))
+    // .pipe(gulp.dest('./script'));
+    //
+    //
+
+
 
 // 默认任务
 gulp.task('default', function(){
