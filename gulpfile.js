3d4d40d97677d14a5612494d880619a56fbc860f
lingjuan.guan <lingjuan.guan@qunar.com>
2015-12-16 00:47:56 +0800

    add

--- gulpfile.js
+++ gulpfile.js
@@ -3,43 +3,26 @@ var browserify = require("browserify");
 var sourcemaps = require("gulp-sourcemaps");
 var source = require('vinyl-source-stream');
 var buffer = require('vinyl-buffer');
+var gutil = require('gulp-util');
 
 gulp.task("browserify", function () {
     var b = browserify({
-        entries: "src/scripts/page/login.js",
+        entries: "./src/scripts/page/login.js",
         debug: true
     });
-    console.log(b);
     return b.bundle()
-        .pipe(source("./login.js"))
+        .pipe(source("login.js"))
         .pipe(buffer())
-        .pipe(sourcemaps.init({loadMaps: true}))
-        .pipe(sourcemaps.write("."))
-        .pipe(gulp.dest("src/prd"));
+        // .pipe(sourcemaps.init({loadMaps: true}))
+        .on('error', gutil.log)
+        .pipe(sourcemaps.write("./"))
+        .pipe(gulp.dest("./src/prd"));
 });
-
-
-	// var b = browserify({
-	//     entries: './script/app.js',
-	//     debug: true
-	// });
-    //
-	// return b.bundle()
-    // .pipe(source('./bundle.js'))
-    // .pipe(buffer())
-    // .pipe(sourcemaps.write('./'))
-    // .pipe(gulp.dest('./script'));
-    //
-    //
-
-
 
 // 默认任务
 gulp.task('default', function(){
-    // gulp.run('lint', 'sass', 'scripts');
-    console.log("开始构建");
     // 监听文件变化
-    gulp.watch('./js/*.js', function(){
+    // gulp.watch('.//*.js', function(){
         gulp.run('browserify');
-    });
+    // });
 });
--- src/scripts/page/login.js
+++ src/scripts/page/login.js
@@ -3,3 +3,4 @@ console.log("aa");
 console.log(c.name);
 var a = require('amazeui');
 console.log(a);
+console.log();
